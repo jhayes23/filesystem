ac6e019d4703d6afa0ccb631e51d79828512f6c0
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2023-04-08 22:23:19 -0700

    fixed headers

--- fsInit.c
+++ fsInit.c
@@ -1,6 +1,6 @@
 /**************************************************************
  * Class:  CSC-415-01 Spring 2023
- * Names: Anthony Benjamin, Nyan Ye Lin, David Chen, Joshua Hayes
+ * Names: Anthony Benjamin, Nyan Ye Lin, Joshua Hayes, David Chen
  * Student IDs: 921119898, 921572181, 922379312
  * GitHub Name: copbrick
  * Group Name: Team DALJ
