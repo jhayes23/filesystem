53636bcfec9f0d9f4815be47d2f276e2c013f7e1
Student <rainiser@icloud.com>
2023-04-18 11:44:27 -0700

    Tested parsePath

--- parsePath.c
+++ parsePath.c
@@ -14,77 +14,67 @@
  *
  **************************************************************/
 // /home/student/Documents/myfile
-//LOAD ROOT DIR
-//LOOK FOR ENTRY WHOSE NAME IS HOME
-//LOAD DATA BLOB that is the home directory (LOAD Directory function)
-//LOOK FOR student
-//LOAD student  if student exist and is directory
-//LOOK for Documents
-//LOAD Documents
-//LOOK FOR myfile
 
-//yes valid path
-//last component  -> exist/doesn't exist -> if exists its a dir or file
-
-//return whether or not its a valid path ( primary components are not valid). if it exists which directory entry index is it
-// ptr * to loaded directory
-//if not exists, index = -1
-//returns parent directory of the index
 #include <stdio.h>
 #include "mfs.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include "directoryEntry.h"
 #include "fsLow.h"
+#include "vcb.h"
+#include "parsePath.h"
 
-
-void parsePath(char * path){
-
-    char pathname[256];
-    strcpy(pathname,path);
-
-    directoryEntry * dir;
-    char* token;
-    char* rest;
-    token = strtok_r(pathname,"/",&rest);
-
-    while(token != NULL){
-        printf("Token: %s\n",token);
-
-        int index = locateEntry(dir,token);
-        if(index == -1 || dir[index].isFile == 0){
-            printf("Does not exist or is file.\n");
-            //return
-        }
-        loadDirectory(dir,dir[index]);
-        token = strtok_r(NULL,"/",&rest);
-    }
-    //do return
-}
-
-void loadDirectory(directoryEntry * loadDir, directoryEntry dir){
-    LBAread(loadDir,dir.fileSize/512,dir.location); //TODO find blockCount after git pull
+void loadDirectory(directoryEntry * loadDir, directoryEntry * dir){
+    LBAread(loadDir,dir->fileSize/vcb->blockSize,dir->location);
 }
 
 int locateEntry( directoryEntry * dir, char * entry){ // search directory, entry = name
-    for (int i = 0; i < (dir->fileSize)/sizeof(directoryEntry); i++)//TODO hopefully num of entries
+    for (int i = 0; i < (dir->fileSize)/sizeof(directoryEntry); i++)
     {
-        printf("I: %d   EntryName: [%s]\n",i,dir->fileName);
-        if(strcmp(dir[i].fileName,entry) == 0)
-        return i;
+        printf("I: %d   EntryName: [%s]\n",i,dir[i].fileName);
+        if(strcmp(dir[i].fileName,entry) == 0){
+            printf("FOUND: %s\n",dir[i].fileName);
+            return i;
+        }
     }
     return -1;
 }
 
-int main() {
-    // char* path = "home/documents/student/file";
-    // parsePath(path);
-    // for(int i = 0; i < 52; i++) {
-    //     for(int j = 0; j < 256; j++) {
-    //         printf("%c", patharray[i][j]);
-    //     }
-    //     printf("\n");
-    // }
 
-    return 0;
+parsedPath parsePath(char * path, directoryEntry * startPath){
+
+    char pathname[256];
+    strcpy(pathname,path);
+   
+   printf("Path to parse: %s\n",pathname);
+   directoryEntry * dir = startPath;
+   if(startPath == NULL){
+    dir = vcb->rootDir;
+   }else{
+    dir = startPath;
+   }
+    
+    char* token;
+    char* rest;
+    token = strtok_r(pathname,"/",&rest);
+    int entryIndex = -1;
+    parsedPath pathRet;
+
+    while(token != NULL){
+        printf("Token: %s\n",token);
+
+        entryIndex = locateEntry(dir,token);
+        token = strtok_r(NULL,"/",&rest);
+        if(entryIndex == -1 || (dir[entryIndex].isFile == FILE && token != NULL)){
+            
+                pathRet.index = entryIndex;
+                pathRet.parent = NULL;
+            return pathRet;
+        }
+        loadDirectory(dir,dir+entryIndex);
+    }
+    
+    pathRet.index = entryIndex;
+    pathRet.parent = dir;
+    return pathRet;
 }
